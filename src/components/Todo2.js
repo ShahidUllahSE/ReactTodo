5a8ad0c443e2e5d3abebc689a5d6853d592f0403
ShahidUllahSE <shahidbangash8899@gmail.com>
2023-11-24 23:08:47 +0500

    made code clean and formated

--- src/components/Todo2.js
+++ src/components/Todo2.js
@@ -6,106 +6,81 @@ import { FaTrashAlt } from "react-icons/fa";
 
 const Todo2 = () => {
 
-    const [input, setInput] = useState( '' );
-    const [items, setItems] = useState ( [] )
-    const [input2, setInput2] = useState ( '' )
-    const [time, setTime] = useState ( [] )
-    
+    const [input, setInput] = useState('');
+    const [items, setItems] = useState([])
+    const [input2, setInput2] = useState('')
+    const [time, setTime] = useState([])
 
     const handleChange = (e) => {
 
         setInput(e.target.value)
-        
     }
 
-    const handleChange2 = ( e ) => {
+    const handleChange2 = (e) => {
         setInput2(e.target.value)
-         
     }
 
     const handleAdd = (e) => {
         e.preventDefault();
         setItems([...items, input])
-        setTime ( [...time, input2 ] )
+        setTime([...time, input2])
         setInput('')
         setInput2('')
     }
 
-
-    
-    const handleAdd2 = ( e ) => {
-        e.preventDefault () ;
-        setTime( ...time, input2 )
-
-      
-        
-        
-        
-    }
-
     const handleDelete = (id) => {
-        const updatedItems = items.filter( ( elem, ind ) => {
+        const updatedItems = items.filter((elem, ind) => {
 
             return ind !== id;
         })
-        const updatedTime = time.filter ( ( elem, ind ) => {
-            return ind !== id ; 
-        } )
+        const updatedTime = time.filter((elem, ind) => {
+            return ind !== id;
+        })
 
-
-        setItems ( updatedItems )
-        setTime  ( updatedTime )
+        setItems(updatedItems)
+        setTime(updatedTime)
     }
 
     const handleReset = () => {
 
-        setItems ( [] )
-        setTime ( [] )
-    } 
-
-  
-   
-
+        setItems([])
+        setTime([])
+    }
     return (
 
         <div className='main-div'>
 
-            <figure className='image'>
-                <img src={images} alt='logo' />
-                <figcaption> <h4> Add your Tasks here </h4> </figcaption>
-            </figure>
+                 <figure className='image'>
+                     <img src={images} alt='logo' />
+                     <figcaption> <h4> Add your Tasks here </h4> </figcaption>
+                 </figure>
 
-            <div className='child-div'>
-                <form onSubmit={handleAdd}>
-                <span> <input className='inputData' type='text' name='enterTask' placeholder='Enter Task' value={input} onChange={handleChange} /> 
-                {/* <FaPlus className='trash' onClick={handleAdd} /> */}
-                 </span>
-                
-                {/* <GrPowerReset className='trash' onClick={handleReset} /> */}
-             
-                {/* <span> <input className='inputData' type='text' name='enterTask' placeholder='Enter Task' value={input} onChange={handleChange} />   </span> */}
+         <div className='child-div'>
+                    <form onSubmit={handleAdd}>
+                    <span> <input className='inputData' type='text' name='enterTask' placeholder='Enter Task' value={input} onChange={handleChange} />
+                        {/* <FaPlus className='trash' onClick={handleAdd} /> */}
+                    </span>
+                    {/* can also use Reset react icon instead of button */}
+                    {/* <GrPowerReset className='trash' onClick={handleReset} /> */}
 
                 </form>
 
                 <div>
 
-            <form onSubmit = { handleAdd }>
+                    <form onSubmit={handleAdd}>
 
-        <input className='inputData' title='Enter Task Duration Here' type='text' name='enterDuration' placeholder='Duration Of task in Mnts'value={input2} onChange={handleChange2 }/>
-
-            
-            </form>
+                    <input className='inputData' title='Enter Task Duration Here' type='text' name='enterDuration' placeholder='Duration Of task in Mnts' value={input2} onChange={handleChange2} />
+                </form>
 
                 </div>
 
-                <button className ='add' onClick = {handleAdd} > Add item </button>
+                <button className='add' onClick={handleAdd} > Add item </button>
 
-                <button className = 'add' onClick = {handleReset} > Reset </button>
-              
+                <button className='add' onClick={handleReset} > Reset </button>
+
             </div>
 
             {
-
                 items.map((item, ind) => {
 
                     return (
@@ -113,40 +88,23 @@ const Todo2 = () => {
                         <div className='ListItems' key={ind}>
 
                             <ul>
-                         <li>
+                                <li>
 
-                         {item} - {time[ind]} mins
+                                    {item} - {time[ind]} mins
 
-
-
-                     
-                            <FaTrashAlt className='trash' onClick={()=>handleDelete(ind)} />
-
-
-                        </li>
-                        
-                     
-
+                                    <FaTrashAlt className='trash' onClick={() => handleDelete(ind)} />
+                                </li>
                             </ul>
 
-                            
-
-
+                                {/* can also use button for reset instead of trash icon */}
                             {/* <h3>{item}</h3> */}
                             {/* <button className='add' onClick={() => handleDelete(ind)}> Delete Item </button> */}
+
+
                         </div>
 
                     )
-                })
-
-
-            }
-
-
-                
-           
-
-
+                })}
 
         </div>
     )
