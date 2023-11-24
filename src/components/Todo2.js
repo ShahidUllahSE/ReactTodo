a6dfec4342122ff26d761192652e62cb4e55c22f
ShahidUllahSE <shahidbangash8899@gmail.com>
2023-11-24 12:38:23 +0500

    added buttons functionality

--- src/components/Todo2.js
+++ src/components/Todo2.js
@@ -2,15 +2,33 @@ import React, { useState } from 'react'
 import images from './images.jpeg'
 
 const Todo2 = () => {
-    
+
     const [input, setInput] = useState('');
-  const  handleChange=(e)=>{
+    const [items, setItems] = useState([])
+    const handleChange = (e) => {
 
         setInput(e.target.value)
-        
+
     }
+
+    const handleAdd = (e) => {
+        setItems([...items, input])
+        setInput('')
+
+
+    }
+
+    const handleDelete = (id) => {
+        const updatedItems = items.filter((elem, ind) => {
+
+            return ind !==id;
+        })
+
+        setItems(updatedItems)
+    }
+
     return (
-    
+
         <div className='main-div'>
 
             <figure>
@@ -22,11 +40,38 @@ const Todo2 = () => {
 
 
                 <span> <input className='inputData' type='text' name='enterTask' placeholder='Enter Task' value={input} onChange={handleChange} />   </span>
-                <button className='add'> Add item </button>
+                <button className='add' onClick={handleAdd} > Add item </button>
+               
+
+
 
             </div>
 
+
+
+
+
+            {
+
+      items.map((item, ind) => {
+                    
+                    
+                    
+                    return ( 
+                        <div key={ind}> 
           
+                    
+                    <h3>{item}</h3>
+                    {/* // <i className='far fa-trash-alt add-btn' title='deleteItem' onClick={()=>handleDelete(ind)}></i> */}
+                     <button className='add' onClick={() => handleDelete(ind)}> Delete Item </button>
+                    </div>
+
+                    )
+
+
+                })
+
+            }
 
 
 
